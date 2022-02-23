3fed9fe3221bb9c507a77ec504a8996ed32de9a0
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-23 19:49:45 +0100

    login init

---
+++ pages/login.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import { Fragment } from "react";
+
+const Login: NextPage = () => {
+  return (
+    <Fragment>
+      <div className="jumbotron text-center bg-primary square">Login</div>
+      <p>login page</p>
+    </Fragment>
+  );
+};
+
+export default Login;
