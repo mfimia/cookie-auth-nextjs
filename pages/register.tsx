4f9b1f6c464c0e86573b5b386a637978be955007
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-23 19:49:58 +0100

    register component init

---
+++ pages/register.tsx
@@ -0,0 +1,53 @@
+import { NextPage } from "next";
+import { FormEvent, Fragment, useState } from "react";
+
+const Register: NextPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.table({ name, email, password });
+  };
+
+  return (
+    <Fragment>
+      <h1 className="jumbotron text-center bg-primary square">Register</h1>
+      <div className="container col-md-4 offset-md-4 pb-5">
+        <form autoComplete="off" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="form-control mb-4 p-4"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter name"
+            required
+          />
+          <input
+            type="email"
+            className="form-control mb-4 p-4"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter email"
+            required
+          />
+          <input
+            type="password"
+            className="form-control mb-4 p-4"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter password"
+            required
+          />
+          <br />
+          <button type="submit" className="btn btn-block btn-primary p-2">
+            Submit
+          </button>
+        </form>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Register;
