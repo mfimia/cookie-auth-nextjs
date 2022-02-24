8c01ce708eea14ee5674da2b2872e247f39e0fcc
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-24 19:56:44 +0100

    provide auth context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,19 @@
 import "antd/dist/antd.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from "next/app";
-import { Fragment } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import TopNav from "../components/TopNav";
+import { Provider } from "../context/index";
 import "../public/css/styles.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <Fragment>
+    <Provider>
       <ToastContainer position="top-center" />
       <TopNav />
       <Component {...pageProps} />
-    </Fragment>
+    </Provider>
   );
 };
 
