515109cbba05b13c4b8ffd0bb142214539a38249
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-24 16:26:16 +0100

    active link from location pathname

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -1,28 +1,47 @@
-import React from "react";
-import { Menu } from "antd";
-import Link from "next/link";
 import {
   AppstoreOutlined,
   LoginOutlined,
   UserAddOutlined,
 } from "@ant-design/icons";
+import { Menu } from "antd";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
 const { Item } = Menu;
 
 const TopNav = () => {
+  const [currentLink, setCurrentLink] = useState("");
+
+  //   set link to current location when refresh or location changes
+  useEffect(() => {
+    typeof window !== "undefined" && setCurrentLink(window.location.pathname);
+  }, [typeof window !== "undefined" && window.location.pathname]);
+
   return (
-    <Menu mode="horizontal">
-      <Item icon={<AppstoreOutlined />}>
+    <Menu mode="horizontal" selectedKeys={[currentLink]}>
+      <Item
+        onClick={(e) => setCurrentLink(e.key)}
+        key={"/"}
+        icon={<AppstoreOutlined />}
+      >
         <Link href={"/"}>
           <a>Home</a>
         </Link>
       </Item>
-      <Item icon={<LoginOutlined />}>
+      <Item
+        onClick={(e) => setCurrentLink(e.key)}
+        key={"/login"}
+        icon={<LoginOutlined />}
+      >
         <Link href={"/login"}>
           <a>Login</a>
         </Link>
       </Item>
-      <Item icon={<UserAddOutlined />}>
+      <Item
+        onClick={(e) => setCurrentLink(e.key)}
+        key={"/register"}
+        icon={<UserAddOutlined />}
+      >
         <Link href={"/register"}>
           <a>Register</a>
         </Link>
