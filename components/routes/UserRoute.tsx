b3dbe0420c287a1bffe0bdd2d6455a954bc9f17f
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-25 18:07:59 +0100

    private routes wrapper

---
+++ components/routes/UserRoute.tsx
@@ -0,0 +1,39 @@
+import { SyncOutlined } from "@ant-design/icons";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { Fragment, ReactNode, useEffect, useState } from "react";
+
+const UserRoute = ({ children }: { children: ReactNode }) => {
+  const [ok, setOk] = useState(false);
+
+  const router = useRouter();
+
+  const fetchUser = async () => {
+    try {
+      const { data } = await axios.get("/api/auth/current-user");
+      if (data.ok) setOk(true);
+    } catch (err) {
+      setOk(false);
+      router.push("/login");
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return (
+    <Fragment>
+      {!ok ? (
+        <SyncOutlined
+          spin
+          className="d-flex justify-content-center display-1 text-primary p-5"
+        />
+      ) : (
+        <Fragment>{children}</Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+export default UserRoute;
